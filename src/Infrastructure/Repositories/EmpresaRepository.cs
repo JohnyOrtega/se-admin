a519cb5c5234988260ded8d478cf587d4994c24c
Renan Ortega <renanortega82@gmail.com>
2024-12-12 18:57:52 -0300

    feat: add empresa filters.

--- src/Infrastructure/Repositories/EmpresaRepository.cs
+++ src/Infrastructure/Repositories/EmpresaRepository.cs
@@ -13,6 +13,30 @@ public class EmpresaRepository(AppDbContext context) : Repository<Empresa>(conte
     {
         var query = _empresas.AsQueryable();
 
+        if (!string.IsNullOrEmpty(filters.FantasyName))
+            query = query.Where(m => m.FantasyName.Contains(filters.FantasyName));
+
+        if (!string.IsNullOrEmpty(filters.Category))
+            query = query.Where(m => m.Category.Contains(filters.Category));
+
+        if (!string.IsNullOrEmpty(filters.Telephone))
+            query = query.Where(m => m.Telephone.Contains(filters.Telephone));
+
+        if (!string.IsNullOrEmpty(filters.Sector))
+            query = query.Where(m => m.Sector.Contains(filters.Sector));
+
+        if (!string.IsNullOrEmpty(filters.SocialReason))
+            query = query.Where(m => m.SocialReason.Contains(filters.SocialReason));
+
+        if (filters.CreatedAt.HasValue)
+            query = query.Where(m => m.CreatedAt.Date == filters.CreatedAt.Value.Date);
+
+        if (filters.UpdatedAt.HasValue)
+            query = query.Where(m => m.UpdatedAt != null && m.UpdatedAt.Value.Date == filters.UpdatedAt.Value.Date);
+
+        if (!string.IsNullOrEmpty(filters.UpdatedBy))
+            query = query.Where(m => m.UpdatedBy == filters.UpdatedBy);
+
         return query;
     }
 
