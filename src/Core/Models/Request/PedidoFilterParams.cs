9fee85b0acf63e25525d648405e947073dcc8f49
Renan Ortega <renanortega82@gmail.com>
2024-12-22 23:55:15 -0300

    feat: add pedido filters.

--- src/Core/Models/Request/PedidoFilterParams.cs
+++ src/Core/Models/Request/PedidoFilterParams.cs
@@ -2,5 +2,24 @@ namespace Core.Models.Request;
 
 public class PedidoFilterParams : PaginationParams
 {
-    
+    public string? Performer { get; set; }
+    public string? Expander { get; set; }
+    public string? Coordinator { get; set; }
+    public DateTime? EntryDate { get; set; }
+    public DateTime? DeliveryDate { get; set; }
+    public string? Client { get; set; }
+    public string? Order { get; set; }
+    public string? PropertyType { get; set; }
+    public int? ParkingSpaces { get; set; }
+    public string? Status { get; set; }
+    public string? ZeroPoint { get; set; }
+    public string? PropertyValue { get; set; }
+    public bool? OnlineCreated { get; set; }
+    public DateTime? OnlineDate { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public bool? MappingCompleted { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
 }
--- src/Infrastructure/Repositories/PedidoRepository.cs
+++ src/Infrastructure/Repositories/PedidoRepository.cs
@@ -12,7 +12,67 @@ public class PedidoRepository(AppDbContext context) : Repository<Pedido>(context
     public IQueryable<Pedido> GetWithFilters(PedidoFilterParams filters)
     {
         var query = _pedidos.AsQueryable();
+
+        if (!string.IsNullOrEmpty(filters.Performer))
+            query = query.Where(p => p.Performer.Contains(filters.Performer));
         
+        if (!string.IsNullOrEmpty(filters.Expander))
+            query = query.Where(p => p.Expander.Contains(filters.Expander));
+        
+        if (!string.IsNullOrEmpty(filters.Coordinator))
+            query = query.Where(p => p.Coordinator.Contains(filters.Coordinator));
+        
+        if (filters.EntryDate.HasValue)
+            query = query.Where(p => p.EntryDate.Date == filters.EntryDate.Value.Date);
+
+        if (filters.DeliveryDate.HasValue)
+            query = query.Where(p => p.DeliveryDate.Date == filters.DeliveryDate.Value.Date);
+
+        if (!string.IsNullOrEmpty(filters.Client))
+            query = query.Where(p => p.Client.Contains(filters.Client));
+        
+        if (!string.IsNullOrEmpty(filters.Order))
+            query = query.Where(p => p.Order.Contains(filters.Order));
+        
+        if (!string.IsNullOrEmpty(filters.PropertyType))
+            query = query.Where(p => p.PropertyType.Contains(filters.PropertyType));
+        
+        if (filters.ParkingSpaces.HasValue)
+            query = query.Where(p => p.ParkingSpaces == filters.ParkingSpaces.Value);
+
+        if (!string.IsNullOrEmpty(filters.Status))
+            query = query.Where(p => p.Status.Contains(filters.Status));
+       
+        if (!string.IsNullOrEmpty(filters.ZeroPoint))
+            query = query.Where(p => p.ZeroPoint.Contains(filters.ZeroPoint));
+        
+        if (!string.IsNullOrEmpty(filters.PropertyValue))
+            query = query.Where(p => p.PropertyValue.Contains(filters.PropertyValue));
+        
+        if (filters.OnlineCreated.HasValue)
+            query = query.Where(p => p.OnlineCreated.Equals(filters.OnlineCreated));
+        
+        if (filters.OnlineDate.HasValue)
+            query = query.Where(p => p.OnlineDate.Date == filters.OnlineDate.Value.Date);
+        
+        if (!string.IsNullOrEmpty(filters.City))
+            query = query.Where(p => p.City.Contains(filters.City));
+        
+        if (!string.IsNullOrEmpty(filters.State))
+            query = query.Where(p => p.State.Contains(filters.State));
+        
+        if (filters.MappingCompleted.HasValue)
+            query = query.Where(p => p.MappingCompleted.Equals(filters.MappingCompleted));
+
+        if (!string.IsNullOrEmpty(filters.UpdatedBy))
+            query = query.Where(m => m.UpdatedBy == filters.UpdatedBy);
+
+        if (filters.CreatedAt.HasValue)
+            query = query.Where(p => p.CreatedAt.Date == filters.CreatedAt.Value.Date);
+        
+        if (filters.UpdatedAt.HasValue)
+            query = query.Where(p => p.UpdatedAt != null && p.UpdatedAt.Value.Date == filters.UpdatedAt.Value.Date);
+
         return query;
     }
 
