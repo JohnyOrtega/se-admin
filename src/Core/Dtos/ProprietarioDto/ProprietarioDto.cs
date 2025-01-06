69a5302c1ba31f5c174fc8ec606393209c2c6868
Renan Ortega <renanortega82@gmail.com>
2025-01-06 14:45:54 -0300

    fix: adjust neighborhood.

--- src/Core/Dtos/ProprietarioDto/ProprietarioDto.cs
+++ src/Core/Dtos/ProprietarioDto/ProprietarioDto.cs
@@ -9,7 +9,7 @@ public class ProprietarioDto
     public string Source { get; set; } = string.Empty;
     public string Telephone { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
-    public string Neighboor { get; set; } = string.Empty;
+    public string Neighborhood { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
     public string State { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
